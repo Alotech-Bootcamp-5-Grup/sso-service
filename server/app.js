e9e6492008d708e30fd5d759b86443c78819ca6c
eren <erentnr@yandex.com>
2021-12-28 00:54:53 +0300

    Add logging with winston

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const authRoute = require("./routes/routes");
+const logger = require("./config/winston");
+const createLogTable = require("./config/logTable");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(express.json());
 
+// middlewares
+app.use(express.json());
 app.use(cors());
+
+// routes
 app.use("/", authRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
 // app listen after database table created
 createLogTable(()=>{
   app.listen(PORT, () => {
